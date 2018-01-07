ebc8894437a79034951f2945e3bd231dc5e08f94
DenLahpai <den.lahpai@icloud.com>
2018-01-07 23:09:51 +0630

    sfd

--- cities.php
+++ cities.php
@@ -62,68 +62,76 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
             include "includes/nav.html";
             include "includes/header.html";
             ?>
-            <main>
-                <form class="form table small" action="#" method="post">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>
-                                    <label for="AirportCode">Airport Code:</label>
-                                    <input type="text" name="AirportCode" id="AirportCode"
-                                    size="3" maxlength="3">
-                                </th>
-                                <th>
-                                    <label for="City">City:</label>
-                                    <input type="text" name="City" id="City" placeholder="City Name">
-                                </th>
-                                <th>
-                                    <label for="CountryCode">Country:</label>
-                                    <select name="CountryCode">
-                                        <option value="">Select</option>
-                                        <?php
-                                        //getting data from the table Countries
-                                        $getRows_Countries = new Database();
-                                        $query_getRows_Countries = "SELECT * FROM Countries ORDER BY Country;";
-                                        $getRows_Countries->query($query_getRows_Countries);
-                                        $rows_Countries = $getRows_Countries->resultset();
-                                        foreach ($rows_Countries as $row_Countries) {
-                                            echo "<option value=\"$row_Countries->Code\">".$row_Countries->Country."</option>";
-                                        }
-                                        ?>
-                                    </select>
-                                </th>
-                                <th>
-                                    <input type="submit" name="buttonSubmit" value="Save">
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            //getting data from the table Cities
-                            $getRows_Cities = new Database();
-                            $query_getRows_Cities = "SELECT
-                                Cities.Id,
-                                Cities.AirportCode,
-                                Cities.City,
-                                Countries.Country AS Country
-                                FROM Cities, Countries
-                                WHERE Cities.CountryCode = Countries.Code
-                                ORDER BY City ;";
-                            $getRows_Cities->query($query_getRows_Cities);
-                            $rows_Cities = $getRows_Cities->resultset();
-                            foreach ($rows_Cities as $row_Cities) {
-                                echo "<tr>";
-                                echo "<td>".$row_Cities->AirportCode."</td>";
-                                echo "<td>".$row_Cities->City."</td>";
-                                echo "<td>".$row_Cities->Country."</td>";
-                                echo "<td><a href=\"citiesEdit.php?CitiesId=$row_Cities->Id\">";
-                                echo "Edit</a></td>";
-                                echo "</tr>";
-                            }
-                            ?>
-                        </tbody>
-                    </table>
+            <section>
+                <form class="form cities" id="form_cities" action="#" method="post">
+                    <ul>
+                        <li>
+                            <label for="AirportCode">Airport Code:</label>
+                            <input type="text" name="AirportCode" id="AirportCode"
+                            size="3" maxlength="3" required>
+                        </li>
+                        <li>
+                            <label for="City">City:</label>
+                            <input type="text" name="City" id="City" placeholder="City Name" required>
+                        </li>
+                        <li>
+                            <label for="CountryCode">Country:</label>
+                            <select name="CountryCode"> -->
+                                <option value="">Select</option>
+                                <?php
+                                //getting data from the table Countries
+                                $getRows_Countries = new Database();
+                                $query_getRows_Countries = "SELECT * FROM Countries ORDER BY Country;";
+                                $getRows_Countries->query($query_getRows_Countries);
+                                $rows_Countries = $getRows_Countries->resultset();
+                                foreach ($rows_Countries as $row_Countries) {
+                                    echo "<option value=\"$row_Countries->Code\">".$row_Countries->Country."</option>";
+                                }
+                                ?>
+                            </select>
+                        </li>
+                        <li>
+                            <button type="submit" name="buttonSubmit">Submit</button>
+                        </li>
+                    </ul>
                 </form>
+            </section>
+            <main>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Code</th>
+                            <th>City</th>
+                            <th>Country</th>
+                            <th>#</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        //getting data from the table Cities
+                        $getRows_Cities = new Database();
+                        $query_getRows_Cities = "SELECT
+                            Cities.Id,
+                            Cities.AirportCode,
+                            Cities.City,
+                            Countries.Country AS Country
+                            FROM Cities, Countries
+                            WHERE Cities.CountryCode = Countries.Code
+                            ORDER BY City ;";
+                        $getRows_Cities->query($query_getRows_Cities);
+                        $rows_Cities = $getRows_Cities->resultset();
+                        foreach ($rows_Cities as $row_Cities) {
+                            echo "<tr>";
+                            echo "<td>".$row_Cities->AirportCode."</td>";
+                            echo "<td>".$row_Cities->City."</td>";
+                            echo "<td>".$row_Cities->Country."</td>";
+                            echo "<td><a href=\"citiesEdit.php?CitiesId=$row_Cities->Id\">";
+                            echo "Edit</a></td>";
+                            echo "</tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
             </main>
         </div><!-- end of content -->
         <?php include "includes/footer.html"; ?>
