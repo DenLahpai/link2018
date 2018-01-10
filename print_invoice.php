4ee76dc32b5c70f294017c0ce0841bed7db50889
DenLahpai <den.lahpai@icloud.com>
2018-01-10 22:52:09 +0630

    Starting printInvoice

---
+++ print_invoice.php
@@ -0,0 +1,16 @@
+<?php
+require "functions.php";
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/print.css">
+        <title>Invoice</title>
+    </head>
+    <body>
+        <div class="content"><!-- content -->
+            <?php include "includes/print_header.html"; //TODO RESUME HERE! ?>
+        </div><!-- end of content -->
+    </body>
+</html>
