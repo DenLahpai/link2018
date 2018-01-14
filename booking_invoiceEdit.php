d157e07dc9ac504342d61a9a6ec62ecb0b324ac9
DenLahpai <den.lahpai@icloud.com>
2018-01-14 20:12:34 +0630

    before changing booking summary

--- booking_invoiceEdit.php
+++ booking_invoiceEdit.php
@@ -32,6 +32,81 @@ else {
     $currency = 'USD';
 }
 
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    //Updating Invoice Header
+    $Addressee = trim($_REQUEST['Addressee']);
+    $Address = trim($_REQUEST['Address']);
+    $City = trim($_REQUEST['City']);
+    $Attn = trim($_REQUEST['Attn']);
+
+    $update_InvoiceHeader = new Database();
+    $query_update_InvoiceHeader = "UPDATE InvoiceHeader SET
+        Addressee = :Addressee,
+        Address = :Address,
+        City = :City,
+        Attn = :Attn
+    WHERE InvoiceNo = :InvoiceNo
+    ;";
+    $update_InvoiceHeader->query($query_update_InvoiceHeader);
+    $update_InvoiceHeader->bind(':Addressee', $Addressee);
+    $update_InvoiceHeader->bind(':Address', $Address);
+    $update_InvoiceHeader->bind(':City', $City);
+    $update_InvoiceHeader->bind(':Attn', $Attn);
+    $update_InvoiceHeader->bind(':InvoiceNo', $InvoiceNo);
+    $update_InvoiceHeader->execute();
+
+    //Updating InvoiceDetails
+    $i = 1;
+    while($i <= 20) {
+        $Id = $_REQUEST["Id$i"];
+        $Date = $_REQUEST["Date$i"];
+        $Description = trim($_REQUEST["Description$i"]);
+        $Amount = $_REQUEST["Amount$i"];
+        $update_InvoiceDetails = new Database();
+        $query_update_InvoiceDetails = "UPDATE InvoiceDetails SET
+            Date = :Date,
+            Description = :Description,
+            $currency = :Amount
+            WHERE Id = :Id
+        ;";
+        $update_InvoiceDetails->query($query_update_InvoiceDetails);
+        $update_InvoiceDetails->bind(':Date', $Date);
+        $update_InvoiceDetails->bind(':Description', $Description);
+        $update_InvoiceDetails->bind(':Amount', $Amount);
+        $update_InvoiceDetails->bind(':Id', $Id);
+        $update_InvoiceDetails->execute();
+        $i++;
+    }
+
+    //getting the SUM
+    $getSum_InvoiceDetails = new Database();
+    $query_getSum_InvoiceDetails = "SELECT SUM($currency) AS $currency FROM InvoiceDetails
+            WHERE InvoiceNo = :InvoiceNo
+    ;";
+    $getSum_InvoiceDetails->query($query_getSum_InvoiceDetails);
+    $getSum_InvoiceDetails->bind(':InvoiceNo', $InvoiceNo);
+    $results = $getSum_InvoiceDetails->resultset();
+    foreach ($results as $result) {
+        $sum = $result->$currency;
+    }
+
+    //Updating data to the table Invoices
+    $InvoiceDate = $_REQUEST['InvoiceDate'];
+    $update_Invoices = new Database();
+    $query_update_Invoice = "UPDATE Invoices SET
+        InvoiceDate = :InvoiceDate,
+        $currency = :sum
+        WHERE InvoiceNo = :InvoiceNo
+    ;";
+    $update_Invoices->query($query_update_Invoice);
+    $update_Invoices->bind(':InvoiceDate', $InvoiceDate);
+    $update_Invoices->bind(':sum', $sum);
+    $update_Invoices->bind(':InvoiceNo', $InvoiceNo);
+    if($update_Invoices->execute()) {
+        header("location: booking_invoiceEdit.php?InvoiceNo=$InvoiceNo");
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -83,7 +158,7 @@ else {
                             <tr>
                                 <td>Attn:</td>
                                 <td>
-                                    <?php echo $Attn; ?>
+                                    <input type="text" name="Attn" value="<?php echo $Attn; ?>">
                                 </td>
                                 <td>Booking Name:</td>
                                 <td>
@@ -91,8 +166,9 @@ else {
                                 </td>
                             </tr>
                             <tr>
+                                <th>Id</th>
                                 <th>Date</th>
-                                <th colspan="2">Service</th>
+                                <th>Service</th>
                                 <th>Amount in <?php echo $currency; ?></th>
                             </tr>
                             <?php
@@ -100,9 +176,13 @@ else {
                             foreach ($rows_InvoiceDetails as $row_InvoiceDetails) {
                                 echo "<tr>";
                                 echo "<td>";
+                                echo "<input type=\"number\" name=\"Id$i\" value=\"$row_InvoiceDetails->Id\"
+                                min=\"1\"max=\"9999\" readonly>";
+                                echo "</td>";
+                                echo "<td>";
                                 echo "<input type=\"date\" name=\"Date$i\" value=\"$row_InvoiceDetails->Date\">";
                                 echo "</td>";
-                                echo "<td colspan=\"2\">";
+                                echo "<td>";
                                 echo "<input type=\"text\" name=\"Description$i\" value=\"$row_InvoiceDetails->Description\">";
                                 echo "</td>";
                                 echo "<td>";
@@ -117,6 +197,13 @@ else {
                                 $i++;
                             }
                             ?>
+                            <tr>
+                                <th colspan="4">
+                                    <button type="submit" name="button">Update</button>
+                                    <a href="<?php echo "print_invoice.php?InvoiceNo=$InvoiceNo&currency=$currency"; ?>" target="_blank">
+                                        <button type="button" name="button">Print</button></a>
+                                </th>
+                            </tr>
                         </tbody>
                     </table>
                 </form>
