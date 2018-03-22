2bf81aec1911bd22cf42bb5a31090e78d32f59c4
Den Lahpai <den@devserver>
2018-03-22 23:19:13 +0630

    radom

--- report_invoices.php
+++ report_invoices.php
@@ -1,10 +1,16 @@
 <?php
 require "functions.php";
 
-if(isset($_REQUEST['buttonInvoiceDate'])) {
+if (isset($_REQUEST['buttonInvoiceDate'])) {
     $InvoiceDate1 = $_REQUEST['InvoiceDate1'];
     $InvoiceDate2 = $_REQUEST['InvoiceDate2'];
     $rows_Invoices = get_InvoiceReport_Filterby_InvoiceDate();
+}
+
+if (isset($_REQUEST['buttonCorporate'])) {
+    $CorporatesId = $_REQUEST['CorporatesId'];
+    $InvoiceStatus = $_REQUEST['InvoicesStatus'];
+    $rows_Invoices = get_InvoiceReport_Filterby_Corporates();
 }
 
 foreach ($rows_Invoices as $row_Invoices) {
@@ -37,6 +43,45 @@ foreach ($rows_Invoices as $row_Invoices) {
                             <input type="date" name="InvoiceDate2" id="InvoiceDate2"
                             value="<?php echo $InvoiceDate2; ?>">
                             <button type="submit" name="buttonInvoiceDate">Submit</button>
+                        </li>
+                        <li>
+                            <label for="CorporatesId">Corporates:</label>
+                            <select id="CorporatesId" name="CorporatesId">
+                                <?php
+                                $rows_Corporates = getRows_Corporates();
+                                foreach ($rows_Corporates as $row_Corporates) {
+                                    if($row_Corporates->Id == $CorporatesId) {
+                                        echo "<option value=\"$row_Corporates->Id\" selected>$row_Corporates->Name</option>";
+                                    }
+                                    else {
+                                        echo "<option value=\"0\">Select One</option>";
+                                        echo "<option value=\"$row_Corporates->Id\">$row_Corporates->Name</option>";
+                                    }
+                                }
+                                ?>
+                            </select>
+                            <select name="InvoicesStatus">
+                            <?php
+                            $get_InvoicesStatus = new Database();
+                            $query_get_InvoicesStatus = "SELECT DISTINCT Status FROM Invoices";
+                            $get_InvoicesStatus->query($query_get_InvoicesStatus);
+                            $rows_InvoicesStatus = $get_InvoicesStatus->resultset();
+                            foreach ($rows_InvoicesStatus as $row_InvoicesStatus) {
+                                if ($row_InvoicesStatus->Status == $InvoiceStatus) {
+                                    echo "<option value=\"$row_InvoicesStatus->Status\" selected>$row_InvoicesStatus->Status</option>";
+                                }
+                                else {
+                                    echo "<option value=\"0\">Select One</option>";
+                                    echo "<option value=\"$row_InvoicesStatus->Status\">$row_InvoicesStatus->Status</option>";
+                                }
+                            }
+                            ?>
+                            </select>
+                            <button type="submit" name="buttonCorporate">Submit</button>
+                        </li>
+                        <li>
+                            <input type="text" name="search" placeholder="Search">
+                            <button type="submit" name="buttonSearch">Search</button>
                         </li>
                     </ul>
                 </form>
